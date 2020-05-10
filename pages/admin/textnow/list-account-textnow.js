7ab8854fed69b7807014683caadfd6842da76590
vuong2017 <phanquyenvuong@gmail.com>
2020-05-10 14:11:46 +0700

    add-lib-ant

--- pages/admin/textnow/list-account-textnow.js
+++ pages/admin/textnow/list-account-textnow.js
@@ -1,11 +1,90 @@
+import { Table, Tag, Space, Card } from 'antd';
+
 import LayoutAdmin from "@/components/Layout/LayoutAdmin";
 import AuthHelper from "@/utils/AuthHelper";
+import { useState } from 'react';
 
 
 const ListAccountTextNow = () => {
+
+    const [columns,] = useState([
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Tags',
+            key: 'tags',
+            dataIndex: 'tags',
+            render: tags => (
+                <>
+                    {tags.map(tag => {
+                        let color = tag.length > 5 ? 'geekblue' : 'green';
+                        if (tag === 'loser') {
+                            color = 'volcano';
+                        }
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Space size="middle">
+                    <a>Invite {record.name}</a>
+                    <a>Delete</a>
+                </Space>
+            ),
+        },
+    ])
+
+    const [data,] = useState([
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+            tags: ['loser'],
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+            tags: ['cool', 'teacher'],
+        },
+    ])
+
     return (
         <LayoutAdmin>
-            <h1>ListAccountTextNow</h1>
+            <Card title="List Account TextNow">
+                <Table columns={columns} dataSource={data} />
+            </Card>
         </LayoutAdmin>
     )
 }
